0df336b95d059745b269302180f3073092c956b9
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-01-29 12:22:15 +0800

    update site

--- conf.py
+++ conf.py
@@ -6,28 +6,18 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Testing"
+project = "PST 0.15.2 Test Website"
 copyright = "2024, Yao Xiao"
 author = "Yao Xiao"
-release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
-
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
+exclude_patterns = ["_build"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
-
-
-html_theme_options = {
-
-}
--- page.rst
+++
@@ -1,6 +0,0 @@
-==========
-Buggy Page
-==========
-
-This is a minimal reproducible example of the bug. The primary sidebar contains
-nothing but still shows up. It is expected to be hidden.
